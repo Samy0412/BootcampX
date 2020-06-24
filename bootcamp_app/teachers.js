e5936b9846865c7d99d0c68156e3863f9bb13939
Samy0412 <samantha.gadet@gmail.com>
2020-06-24 23:41:24 +0000

    queries with Node postgres

---
+++ bootcamp_app/teachers.js
@@ -0,0 +1,29 @@
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  user: "vagrant",
+  password: "123",
+  host: "localhost",
+  database: "bootcampx",
+});
+
+const userInput = process.argv.slice(2);
+
+pool
+  .query(
+    `
+    SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+    FROM teachers
+    JOIN assistance_requests ON teachers.id = teacher_id
+    JOIN students ON students.id = student_id
+    JOIN cohorts ON cohorts.id = cohort_id
+    WHERE cohorts.name = '${userInput[0]}' 
+    ORDER BY teachers.name;
+`
+  )
+  .then((res) => {
+    res.rows.forEach((row) => {
+      console.log(`${row.cohort}: ${row.teacher}`);
+    });
+  })
+  .catch((err) => console.error("query error", err.stack));
